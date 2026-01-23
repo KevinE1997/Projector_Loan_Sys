0848749d99a6c6fcf6ca0b71a017d84dfb2e1328
Kevin Endara <kevinendara1997@gmail.com>
2026-01-22 23:31:45 -0500

    fix: kafka paths

--- apps/client-app/src/app/services/inventory.service.ts
+++ apps/client-app/src/app/services/inventory.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { CreateProjectorDto, Projector } from '../../types/inventory.types';
 
-const API_URL = import.meta.env.VITE_API_URL; 
+const API_URL = 'http://plms-gateway-alb-1194092981.us-east-1.elb.amazonaws.com'; 
 
 // Helper para el token
 const getAuthHeader = () => {
@@ -16,7 +16,7 @@ export const inventoryService = {
   getAllProjectors: async (): Promise<Projector[]> => {
     // Apuntamos a /api/inventory + /projectors (del controlador)
     const response = await axios.get<Projector[]>(
-      `${API_URL}/api/inventory/projectors`, 
+      `${API_URL}/api/projectors`, 
       getAuthHeader()
     );
     return response.data;
@@ -25,7 +25,7 @@ export const inventoryService = {
   // POST: Crear uno nuevo
   createProjector: async (projector: CreateProjectorDto): Promise<Projector> => {
     const response = await axios.post<Projector>(
-      `${API_URL}/api/inventory/projectors`,
+      `${API_URL}/api/projectors`,
       projector,
       getAuthHeader()
     );
@@ -35,7 +35,7 @@ export const inventoryService = {
   // GET: Obtener uno por ID (Opcional, pero tu controlador lo tiene)
   getProjectorById: async (id: string): Promise<Projector> => {
     const response = await axios.get<Projector>(
-      `${API_URL}/api/inventory/projectors/${id}`,
+      `${API_URL}/api/projectors/${id}`,
       getAuthHeader()
     );
     return response.data;
